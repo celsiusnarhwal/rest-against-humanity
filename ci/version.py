99a1ef2d08e2be2ac13354d9e0e55cfb2e1a2bc9
celsius narhwal <hello@celsiusnarhwal.dev>
2023-03-24 22:10:22 -0400

    update push workflow

--- ci/version.py
+++ ci/version.py
@@ -4,4 +4,4 @@ import tomlkit as toml
 version = toml.load(open("pyproject.toml"))["tool"]["poetry"]["version"]
 version = semver.parse_version_info(version)
 
-print(f"DOCS_VERSION={version.major}.{version.major}")
+print(f"DOCS_VERSION={version.major}.{version.minor}")
