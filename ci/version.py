e7991dd181c1ca9682b43c3b88eb2b7a084a55e1
celsius narhwal <hello@celsiusnarhwal.dev>
2023-03-25 08:24:05 -0400

    remove ci workflows

--- ci/version.py
+++
@@ -1,7 +0,0 @@
-import semver
-import tomlkit as toml
-
-version = toml.load(open("pyproject.toml"))["tool"]["poetry"]["version"]
-version = semver.parse_version_info(version)
-
-print(f"DOCS_VERSION={version.major}.{version.minor}")
