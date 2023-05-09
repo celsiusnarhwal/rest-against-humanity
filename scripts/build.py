57099c076cf01548ae7241164525c45a4786e965
celsius narhwal <hello@celsiusnarhwal.dev>
2023-05-08 22:33:46 -0400

    update build.yml

--- scripts/build.py
+++ scripts/build.py
@@ -4,11 +4,11 @@ import subprocess
 from path import Path
 
 
-def build(push: bool):
+def build(branch: str, push: bool):
     with Path.getcwd() / "docs" / "versions":
         for version_dir in Path.getcwd().listdir():
             if not (version := version_dir.stem).startswith("."):
                 subprocess.run(
-                    f"mike deploy -b vercel -F {version}/mkdocs.yml {version} {'--push' if push else ''}".split(),
+                    f"mike deploy -b {branch} - F{version}/mkdocs.yml {version} {'--push' if push else ''}".split(),
                     env=os.environ,
                 )
