f323a4488a9d3f76ac2abf89ad4a9161d4f0ddc9
celsius narhwal <hello@celsiusnarhwal.dev>
2023-05-09 13:56:34 -0400

    update build.py

--- scripts/build.py
+++ scripts/build.py
@@ -1,16 +1,69 @@
 import subprocess
+from typing import ClassVar
 
+import tomlkit as toml
 from path import Path
+from pydantic import BaseModel, Field, validator
+from tomlkit import TOMLDocument
+
+
+class Version(BaseModel):
+    name: str
+    title: str = None
+    aliases: list[str] = Field(default_factory=list)
+    default: bool = False
+
+    # noinspection PyMethodParameters
+    @validator("title", always=True)
+    def set_title(cls, v, values):
+        return v or values["name"]
+
+
+class Mike(BaseModel):
+    versions_path: ClassVar[Path] = Path.getcwd() / "docs" / "versions"
+    branch: str
+
+    def deploy(self, version: str, push: bool):
+        cmd = f"mike deploy -b {self.branch} {version}"
+
+        if push:
+            cmd += " --push --force"
+
+        self._execute(cmd, version)
+
+    def retitle(self, version: str, title: str):
+        cmd = f"mike retitle -b {self.branch} {version} {title}"
+        self._execute(cmd, version)
+
+    def alias(self, version: str, alias: str):
+        cmd = f"mike alias -b {self.branch} {version} {alias}"
+        self._execute(cmd, version)
+
+    def set_default(self, version: str):
+        cmd = f"mike set-default -b {self.branch} {version}"
+        self._execute(cmd, version)
+
+    def _execute(self, cmd: str, version: str):
+        with self.versions_path / version:
+            subprocess.run(cmd.split())
+
+    @classmethod
+    def get_versions(cls) -> TOMLDocument:
+        versions = toml.load((cls.versions_path / "versions.toml").open())
+        return [Version.parse_obj(version) for version in versions]
 
 
 def build(branch: str, push: bool):
-    with Path.getcwd() / "docs" / "versions":
-        for version_dir in Path.getcwd().listdir():
-            if not (version := version_dir.stem).startswith("."):
-                subprocess.run(
-                    f"mike deploy -b {branch} -F {version}/mkdocs.yml {version} {'--push' if push else ''}".split()
-                )
+    mike = Mike(branch=branch)
 
-                subprocess.run(
-                    f"mike set-default -b {branch} {open('.latest').read().strip()}".split()
-                )
+    for version in mike.get_versions():
+        mike.deploy(version.name, push)
+
+        if version.title:
+            mike.retitle(version.name, version.title)
+
+        for alias in version.aliases:
+            mike.alias(version.name, alias)
+
+        if version.default:
+            mike.set_default(version.name)
