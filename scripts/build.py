7193cb4b701446d461faa0aeb5acf0f91c50f235
celsius narhwal <hello@celsiusnarhwal.dev>
2023-05-11 13:20:08 -0400

    update build process

--- scripts/build.py
+++ scripts/build.py
@@ -1,4 +1,6 @@
+import os
 import subprocess
+import uuid
 from typing import ClassVar
 
 import tomlkit as toml
@@ -23,31 +25,40 @@ class Mike(BaseModel):
     versions_path: ClassVar[Path] = Path.getcwd() / "docs" / "versions"
     branch: str
     push: bool
+    debug: bool
 
     def deploy(self, version: str):
-        cmd = ["mike", "deploy", "-b", self.branch, version]
+        cmd = ["mike", "deploy", version]
         self._execute(cmd, version)
 
     def retitle(self, version: str, title: str):
-        cmd = ["mike", "retitle", "-b", self.branch, version, title]
+        cmd = ["mike", "retitle", version, title]
         self._execute(cmd, version)
 
     def alias(self, version: str, alias: str):
-        cmd = ["mike", "alias", "-b", self.branch, version, alias]
+        cmd = ["mike", "alias", version, alias]
         self._execute(cmd, version)
 
     def set_default(self, version: str):
-        cmd = ["mike", "set-default", "-b", self.branch, version]
+        cmd = ["mike", "set-default", version]
         self._execute(cmd, version)
 
     def _execute(self, cmd: list, version: str):
+        cmd.extend(["--branch", self.branch, "--allow-empty"])
+
+        if self.push:
+            cmd.append("--push")
+
+        log = f"Executing: {' '.join(cmd)}"
+
+        if os.getenv("DOPPLER_ENVIRONMENT") == "gh":
+            log = f"::debug::{log}"
+
+        if self.debug or "::debug::" in log:
+            subprocess.run(["echo", log])
+
         with self.versions_path / version:
-            if self.push:
-                cmd.append("--push")
-
-            print(f"Executing: {' '.join(cmd)}")
-
-            subprocess.run(cmd + ["--allow-empty"])
+            subprocess.run(cmd)
 
     @classmethod
     def get_versions(cls) -> TOMLDocument:
@@ -55,8 +66,11 @@ class Mike(BaseModel):
         return parse_obj_as(list[Version], versions["version"])
 
 
-def build(branch: str, push: bool):
-    mike = Mike(branch=branch, push=push)
+def build(branch: str, push: bool, debug: bool):
+    if branch == "__random__":
+        branch = f"build/{uuid.uuid4().hex}"
+
+    mike = Mike(branch=branch, push=push, debug=debug)
 
     for version in mike.get_versions():
         mike.deploy(version.name)
